e55a64bfa2232300942f8ce7153fb5b2f8f58e9b
Роман <daero3333@yandex.ru>
2021-02-09 20:06:01 +0300

    _

--- foodpro/foodpro/settings.py
+++ foodpro/foodpro/settings.py
@@ -67,9 +67,12 @@ WSGI_APPLICATION = 'foodpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # Я хотел бы изменить бд, когда буду использовать докер
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
